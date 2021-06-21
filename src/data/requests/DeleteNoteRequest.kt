c079635fe22a719823ae812f66ca7f2f8f41cffb
Surasak Adulprasertsuk <jojo141a@hotmail.com>
2021-06-22 00:07:56 +0700

    add add-note and delete-note API

---
+++ src/data/requests/DeleteNoteRequest.kt
@@ -0,0 +1,5 @@
+package com.nongmah.data.requests
+
+data class DeleteNoteRequest(
+    val id: String
+)
--- src/routes/NoteRoutes.kt
+++ src/routes/NoteRoutes.kt
@@ -1,15 +1,20 @@
 package com.nongmah.routes
 
+import com.nongmah.data.collections.Note
+import com.nongmah.data.deleteNoteForUser
 import com.nongmah.data.getNotesForUser
+import com.nongmah.data.requests.DeleteNoteRequest
+import com.nongmah.data.saveNote
 import io.ktor.application.call
 import io.ktor.auth.UserIdPrincipal
 import io.ktor.auth.authenticate
 import io.ktor.auth.principal
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.request.*
 import io.ktor.response.respond
-import io.ktor.routing.Route
-import io.ktor.routing.get
-import io.ktor.routing.route
+import io.ktor.routing.*
 
 fun Route.noteRoutes() {
     route("/get-notes") {
@@ -19,6 +24,43 @@ fun Route.noteRoutes() {
 
                 val notes = getNotesForUser(email)
                 call.respond(OK, notes)
+            }
+        }
+    }
+
+    route("/delete-note") {
+        authenticate {
+            post {
+                val email = call.principal<UserIdPrincipal>()!!.name
+                val request = try {
+                    call.receive<DeleteNoteRequest>()
+                } catch (e: ContentTransformationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+                if (deleteNoteForUser(email, request.id)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
+            }
+        }
+    }
+
+    route("/add-note") {
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+                } catch (e: ContentTransformationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+                if (saveNote(note)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
             }
         }
     }
